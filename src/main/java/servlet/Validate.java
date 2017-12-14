9f762042e3c9e691da9b6feb2e9e9c2fd91e1514
dtaylo56 <derek.taylor@optum.com>
2017-12-14 12:37:00 -0600

    httpSession object demo

--- src/main/java/servlet/Validate.java
+++ src/main/java/servlet/Validate.java
@@ -1,10 +1,10 @@
 package servlet;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -13,25 +13,16 @@ public class Validate extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        try {
-            String name = request.getParameter("user");
-            String password = request.getParameter("pass");
 
-            if(password.equals("studytonight"))
-            {
-                RequestDispatcher rd = request.getRequestDispatcher("Welcome");
-                rd.forward(request, response);
-            }
-            else
-            {
-                out.println("<font color='red'><b>You have entered incorrect password</b></font>");
-                RequestDispatcher rd = request.getRequestDispatcher("index.html");
-                rd.include(request, response);
-            }
-        }finally {
-            out.close();
+        String name = request.getParameter("user");
+        String password = request.getParameter("pass");
+
+        if (password.equals("1234")){
+            //creating a session
+            HttpSession session = request.getSession();
+            session.setAttribute("user", name);
+            response.sendRedirect("Welcome");
         }
-
     }
 
 }
--- src/main/java/servlet/Welcome.java
+++ src/main/java/servlet/Welcome.java
@@ -1,26 +1,24 @@
 package servlet;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Welcome extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
-        try {
-            ServletConfig sc=getServletConfig();
-            out.println(sc.getInitParameter("email"));
-        } finally {
-            out.close();
-        }
+        HttpSession session = request.getSession();
+        String user = (String) session.getAttribute("user");
+
+        out.print("Hello " + user);
 
     }
 
