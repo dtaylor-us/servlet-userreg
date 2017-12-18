77d8acb898b1cb6986ba81e6acea035e6a31ef7e
Derek <derektaylor.us@gmail.com>
2017-12-17 22:20:13 -0600

    Add log4j

---
+++ src/main/java/config/ConfigurationConstants.java
@@ -0,0 +1,11 @@
+package config;
+
+public class ConfigurationConstants {
+    public static final String DB_URL = "jdbc:mysql://localhost:2424/docker_env";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "password";
+    public static final String MAIL_USERNAME = "mail id here"; // like example@outlook.com
+    public static final String MAIL_PASSWORD = "password here";  // your mail password here
+    public static final String MAIL_SMTP_HOST = "smtp password here"; // smtp.live.com
+    public static final String MAIL_REGISTRATION_SITE_LINK = "http://localhost:8080/demos/VerifyRegisteredEmailHash";
+}
